ece6d0e58c5b09b420fbdbac6171b38b8adba9be
kirkinman <101367936+kirkinman@users.noreply.github.com>
2023-07-17 16:58:12 -0400

    Update music_lava.js

--- music_lava.js
+++ music_lava.js
@@ -1,13 +1,19 @@
+var songs = [
+	"igitrdone.mp3",
+	"kick to beats.mp3",
+]
+
 $("h1").click(function() {
 	$("h1").text("lava blast!");
 	$("h1").css("color", "red");
-	
 });
 
 $("button").click(function() {
-	$("h1").text("Sample: High Horse");
-	$("h1").css("color", "blue");
-	var song1 = new Audio("igitrdone.mp3");
+	alert(this.value);
+	var nums = this.innerHTML;
+	var song1 = new Audio(songs[nums]);
 	song1.play();
 	
+	$("h1").text("Sample: High Horse");
+	$("h1").css("color", "blue");	
 });

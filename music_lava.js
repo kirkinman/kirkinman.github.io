dbddb9ed585895ff14792cd19335b5222437833f
kirkinman <101367936+kirkinman@users.noreply.github.com>
2023-07-24 11:56:15 -0400

    Update music_lava.js

--- music_lava.js
+++ music_lava.js
@@ -3,11 +3,22 @@ $("h1").click(function() {
 	$("h1").css("color", "red");
 });
 
+$("img").click(function() {
+	alert("music lava")
+	
+})
+
 $("button").click(function() {
 	var song = this.innerHTML;
 	var song1 = new Audio(song+".mp3");
 	song1.play();
 	
 	$("h1").text("Sample: "+song);
-	$("h1").css("color", "blue");	
+	$("h1").css("color", "blue");
+//	$("img").attr("src" (song"+,mp3")),
+		
+//	})
 });
+
+//##auto make buttons with 
+

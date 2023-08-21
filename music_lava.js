a5a2d2e4931bbc1feacf1aefc0468eca6c5d62fd
kirkinman <mr.kirkinman@icloud.com>
2023-08-21 11:56:14 -0400

    monday updates

--- music_lava.js
+++ music_lava.js
@@ -1,3 +1,6 @@
+var cred = []
+
+
 $("h1").click(function() {
 	$("h1").text("lava blast!");
 	$("h1").css("color", "red");
@@ -6,19 +9,25 @@ $("h1").click(function() {
 $("img").click(function() {
 	alert("music lava")
 	
-})
+});
 
 $("button").click(function() {
 	var song = this.innerHTML;
 	var song1 = new Audio(song+".mp3");
 	song1.play();
 	
-	$("h1").text("Sample: "+song);
+	$("h1").text("Sample: " + song);
 	$("h1").css("color", "blue");
 //	$("img").attr("src" (song"+,mp3")),
-		
+	
+	$(".big").attr('src', song + ".png");
+//	cred.push(song);
+//	alert(cred);
+	
 //	})
 });
 
-//##auto make buttons with 
+//##auto make buttons with this.
+
+
 
